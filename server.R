1271422d54590bfc820c77453a1e5823d3288319
TuomoNieminen <tuomo.a.nieminen@helsinki.fi>
2016-01-25 15:56:27 +0200

    cleaned up code and added comments

--- server.R
+++ server.R
@@ -3,44 +3,33 @@ library(shiny)
 # Define server logic for random distribution application
 shinyServer(function(input, output) {
   
-  # parameters from user
-  mean <- reactive({
-    if(!is.na(input$mean)) input$mean else 0
-  })
-  sd <- reactive({
-    if(!is.na(input$sd)) input$sd else 1
-  })
-  df1 <- reactive({
-    if(!is.na(input$df1)) input$df1 else 3
-  })
-  df2 <- reactive({
-    if(!is.na(input$df2)) input$df2 else 2
-  })
+  # parameters from user 
+  mean <- reactive({if(!is.na(input$mean)) input$mean else 0})
+  sd <- reactive({if(!is.na(input$sd)) input$sd else 1})
+  df1 <- reactive({if(!is.na(input$df1)) input$df1 else 3})
+  df2 <- reactive({if(!is.na(input$df2)) input$df2 else 2})
   
-  # x  
+  # x values for plotting
   x <- reactive({
     switch(input$dist,
            "normal" = seq(-4*sd()+mean(),4*sd()+mean(),length=200),
            "t" = seq(-4,4,length=200),
            "F" = seq(0,9,length=200),
-           "chi-squared" = seq(0,3*df1(),length=200)
-    )
+           "chi-squared" = seq(0,3*df1(),length=200))
   })
   
-  # fx
+  # probability density values for plotting
   fx <- reactive({
     function(x) {
       switch(input$dist,
              "normal" = dnorm(x, mean(),sd()),
              "t" = dt(x, df1()),
              "F" = df(x, df1(), df2()),
-             "chi-squared" = dchisq(x, df1())
-      )
+             "chi-squared" = dchisq(x, df1()))
     }
   })
-
-  
-  # highlighted region ranges
+ 
+  # x values for highlighted probability regions
     hx <- reactive({      
     switch(input$ptype,
            cum = x()[x() <= input$q],
@@ -49,15 +38,14 @@ shinyServer(function(input, output) {
     )
   })
   
-  #probability
+  #probabilities  
   p <- reactive({
     q <- input$q
     switch(input$dist,
            "normal" = pnorm(q, mean(), sd()),
            "t" = pt(q, df1()),
            "F"= pf(q, df1(), df2()),
-           "chi-squared"=pchisq(q, df1())
-    )
+           "chi-squared"=pchisq(q, df1()))
   }) 
   p0 <- reactive({
     q <- input$q0
@@ -65,11 +53,13 @@ shinyServer(function(input, output) {
            "normal" = pnorm(q, mean(), sd()),
            "t" = pt(q, df1()),
            "F"= pf(q,df1(), df2()),
-           "chi-squared"=pchisq(q, df1())
-    )
+           "chi-squared"=pchisq(q, df1()))
   })
   
-  #probability output
+
+# outputs
+  
+  # calculated probability
   output$prob <- renderText({   
     p <- switch(input$ptype,
                 cum = p(),
@@ -80,14 +70,15 @@ shinyServer(function(input, output) {
   })
   
   # density plot
+  
   output$plot <- renderPlot({
     
-    # density
+    # density curve
     plot(x(), fx()(x()), 
          type='l', ylim=c(0,input$ylim),
          main=paste("The density of the",input$dist, "distribution"))
     
-    # p area 
+    # probability area 
     polygon(c( min(hx()), hx(), max(hx()) ),
             c( 0, fx()(hx()) , 0 ), 
             col = "grey")
