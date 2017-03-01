cc96ad94f1faa8a20c59ecf24537a136bf371112
Tuomo Nieminen <tuomo.a.nieminen@helsinki.fi>
2017-03-02 00:32:56 +0200

    add github logo

--- ui.R
+++ ui.R
@@ -4,16 +4,23 @@ library(shiny)
 shinyUI(fluidPage(
   
   # Title
-  h2("Probability calculator", align="center",style="color: #7BB0BC;"),
+  br(),
+  fluidRow(
+           column(11,
+              h2("Probability calculator", align="center",style="color: #7BB0BC;")),
+           column(1, tags$a(img(id ="github", src = 'github.PNG'), 
+                            href = "https://github.com/TuomoNieminen/Calculator"))),
   
   tabsetPanel(id="dist_type",
     tabPanel("Continuous distributions",value="continuous",
-             source("continuous_UI.R", local=T)$value
+             source("ui_continuous.R", local=T)$value
              ),
     tabPanel("Discrete distributions",value="discrete",
-             source("discrete_UI.R", local=T)$value)
-  )
+             source("ui_discrete.R", local=T)$value)
+  ),
   
-
+hr(),
+div(align = "center", "Tuomo Nieminen 2016"),
+br()
 )
 )
